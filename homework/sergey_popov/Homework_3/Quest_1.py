d4b08160ebc4ff1a4ec94d2fc8bf94078c83cc6a
Sergey Popov <popovsv213@yandex.ru>
2025-10-03 21:49:24 +0300

    Homework_3 done

---
+++ homework/sergey_popov/Homework_3/Quest_1.py
@@ -0,0 +1,13 @@
+#Задание 1
+
+a = 4
+b = 2
+
+summ = (a+b)
+diff = (a-b)
+prod = (a*b)
+
+print('a=',a , 'b=' ,b )
+print ('Сумма',summ)
+print ('Разность',diff)
+print ('Произведение',prod)
---
+++ homework/sergey_popov/Homework_3/Quest_4.py
@@ -0,0 +1,11 @@
+# Задание 4
+
+a = 6
+b = 8
+
+hypotenuse = (a**2 + b**2) ** 0.5
+area = (a * b) / 2
+
+print("Катеты:", a, b)
+print("Гипотенуза:", hypotenuse)
+print("Площадь:", area)
